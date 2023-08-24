82e8f4f8dfb281c61995e3469ffb6b620b4aabc2
Patracom <pelomingenieria2024@gmail.com>
2023-08-23 23:29:55 -0400

    cambios en dto crear evento sae

--- src/eventos_sae/dto/create-evento-sae.dto.ts
+++ src/eventos_sae/dto/create-evento-sae.dto.ts
@@ -15,13 +15,13 @@ export class CreateEventoSaeDto {
     @IsString()
     @IsNotEmpty()
     @MaxLength(12)
-    @Matches(/^(\d{1,3}(?:\.\d{3})*)-(\d|K)$/, { message: 'El campo rut_maestro debe tener un formato válido de RUT chileno (ej: 12.345.678-9)' })
+   // @Matches(/^(\d{1,3}(?:\.\d{3})*)-(\d|K)$/, { message: 'El campo rut_maestro debe tener un formato válido de RUT chileno (ej: 12.345.678-9)' })
     rut_maestro: string;
   
     @IsString()
     @IsNotEmpty()
     @MaxLength(12)
-    @Matches(/^(\d{1,3}(?:\.\d{3})*)-(\d|K)$/, { message: 'El campo rut_ayudante debe tener un formato válido de RUT chileno (ej: 12.345.678-9)' })
+   // @Matches(/^(\d{1,3}(?:\.\d{3})*)-(\d|K)$/, { message: 'El campo rut_ayudante debe tener un formato válido de RUT chileno (ej: 12.345.678-9)' })
     rut_ayudante: string;
 
     @IsString()
